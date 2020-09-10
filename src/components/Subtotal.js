176168040d0ad688a4e09eae050c3996a8402bd2
= <=>
2020-09-10 23:52:17 +0500

    v1 done

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import CurrencyFormat from 'react-currency-format';
-import {useStateValue} from '../components/StateProvider';
-import {getBasketTotal} from '../components/Reducer';
+import React from "react";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../components/StateProvider";
+import { getBasketTotal } from "../components/Reducer";
 
-import './Subtotal.css'
+import "./Subtotal.css";
 const Subtotal = () => {
-    const [{basket},dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
 
-    return (
-        <div className="subtotal">
-            <CurrencyFormat
-            renderText={(value)=>
-                <>
-                <p>
-                    Subtotal ({basket.length} items) : <strong>{`${value}`}</strong>
-                </p>
-                <small className="subtoal__gift">
-                    <input type="checkbox"></input> This order contains Gift
-                </small>
-                </>
-            }
-            displayType={"text"}
-            decimalScale={2}
-            prefix={"$"}
-            suffix={"/-"}
-            thousandSeparator={true}	
-            value={getBasketTotal(basket)}
-            />
-            <button>Proceed to Checkout</button>
-        </div>
-    )
-}
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items) : <strong>{`${value}`}</strong>
+            </p>
+            <small className="subtoal__gift">
+              <input type="checkbox"></input> This order contains Gift
+            </small>
+          </>
+        )}
+        displayType={"text"}
+        decimalScale={2}
+        prefix={"$"}
+        suffix={"/-"}
+        thousandSeparator={true}
+        value={getBasketTotal(basket)}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+};
 
-export default Subtotal
+export default Subtotal;
